039ce8e723153c24df7d983fe56879da1c53c393
octavian.voloaca <octavianvoloaca@gmail.com>
2019-06-24 16:49:14 +0300

    fix import

--- elements/iron-views/add-no-link.js
+++ elements/iron-views/add-no-link.js
@@ -1,7 +1,7 @@
 "use strict";
 import {LitElement, html, css} from '/node_modules/lit-element/lit-element.js';
 import {flexLayoutClasses} from "./../flex-layout/flex-layout-classes.js";
-import "./../iron-from/iron-form.js";
+import "./../iron-form/iron-form.js";
 import "./../paper-table/paper-table.js";
 import "./../paper-fab/paper-fab.js";
 import "./../paper-dialog/paper-dialog.js";
