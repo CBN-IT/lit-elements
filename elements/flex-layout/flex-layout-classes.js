8a8e01e4a0f53e483bbe2ca6beaf8262b31e7b22
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2019-10-21 18:02:39 +0300

    cleanup

--- elements/flex-layout/flex-layout-classes.js
+++ elements/flex-layout/flex-layout-classes.js
@@ -9,40 +9,40 @@ export const flexLayoutClasses = css`
         display: -webkit-flex;
         display: flex;
     }
-
+    
     .layout.horizontal {
         -ms-flex-direction: row;
         -webkit-flex-direction: row;
         flex-direction: row;
     }
-
+    
     .layout.vertical {
         -ms-flex-direction: column;
         -webkit-flex-direction: column;
         flex-direction: column;
         min-height: 0;
     }
-
+    
     .layout.wrap {
         -ms-flex-wrap: wrap;
         -webkit-flex-wrap: wrap;
         flex-wrap: wrap;
     }
-
+    
     .layout.center,
     .layout.center-center {
         -ms-flex-align: center;
         -webkit-align-items: center;
         align-items: center;
     }
-
+    
     .layout.center-justified,
     .layout.center-center {
         -ms-flex-pack: center;
         -webkit-justify-content: center;
         justify-content: center;
     }
-
+    
     .flex {
         -ms-flex: 1 1 0.000000001px;
         -webkit-flex: 1;
@@ -50,13 +50,13 @@ export const flexLayoutClasses = css`
         -webkit-flex-basis: 0.000000001px;
         flex-basis: 0.000000001px;
     }
-
+    
     .flex-auto {
         -ms-flex: 1 1 auto;
         -webkit-flex: 1 1 auto;
         flex: 1 1 auto;
     }
-
+    
     .flex-none {
         -ms-flex: none;
         -webkit-flex: none;
@@ -67,57 +67,57 @@ export const flexLayoutClasses = css`
         -webkit-align-items: flex-start;
         align-items: flex-start;
       }
-
-  .layout.center,
-  .layout.center-center {
+    
+    .layout.center,
+    .layout.center-center {
     -ms-flex-align: center;
     -webkit-align-items: center;
     align-items: center;
-  }
-
-  .layout.end {
+    }
+    
+    .layout.end {
     -ms-flex-align: end;
     -webkit-align-items: flex-end;
     align-items: flex-end;
-  }
-
-  .layout.baseline {
+    }
+    
+    .layout.baseline {
     -ms-flex-align: baseline;
     -webkit-align-items: baseline;
     align-items: baseline;
-  }
-
-  /**
-   * Alignment in main axis.
-   */
-  .layout.start-justified {
+    }
+    
+    /**
+    * Alignment in main axis.
+    */
+    .layout.start-justified {
     -ms-flex-pack: start;
     -webkit-justify-content: flex-start;
     justify-content: flex-start;
-  }
-
-  .layout.center-justified,
-  .layout.center-center {
+    }
+    
+    .layout.center-justified,
+    .layout.center-center {
     -ms-flex-pack: center;
     -webkit-justify-content: center;
     justify-content: center;
-  }
-
-  .layout.end-justified {
+    }
+    
+    .layout.end-justified {
     -ms-flex-pack: end;
     -webkit-justify-content: flex-end;
     justify-content: flex-end;
-  }
-
-  .layout.around-justified {
+    }
+    
+    .layout.around-justified {
     -ms-flex-pack: distribute;
     -webkit-justify-content: space-around;
     justify-content: space-around;
-  }
-
-  .layout.justified {
+    }
+    
+    .layout.justified {
     -ms-flex-pack: justify;
     -webkit-justify-content: space-between;
     justify-content: space-between;
-  }
+    }
 `;
