63eed0116dd0978c7b9c11eae6be4da3cd8dfaf5
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2019-10-21 17:46:23 +0300

    confirm-delete and demo

--- demo/paper-table/table-demo.js
+++ demo/paper-table/table-demo.js
@@ -2,6 +2,8 @@
 import {LitElement, html, css} from '/node_modules/lit-element/lit-element.js';
 import {flexLayoutClasses} from "/elements/flex-layout/flex-layout-classes.js";
 import "/elements/paper-table/paper-table.js";
+import "/elements/paper-button/paper-button.js";
+import "/elements/confirm-delete/confirm-delete.js";
 
 class TableDemo extends LitElement {
 
@@ -43,6 +45,13 @@ class TableDemo extends LitElement {
                 "title": "Textarea",
                 "filterable": true,
                 "sortable": true
+            },
+            {
+                "name": "delete",
+                "title": "",
+                "filterable": false,
+                "sortable": false,
+                "template": "html`<div class='vertical layout center'><paper-button style='padding:0;color:var(--red-color);margin:auto' small icon='delete' @click='${(event) => {CBNUtils.fireEvent(this, 'confirm-delete', {url: 'asdasd', body: {asdasd: 'asdasd'}, message: 'Are you sure you want to delete asdasd?'});event.stopPropagation();}}'></paper-button></div>`"
             }
         ];
         let items = new Array(1000).fill(5);
@@ -66,7 +75,7 @@ class TableDemo extends LitElement {
                 }                             
             </style>
             <paper-table class="flex" .columns="${this.columns}" .items="${this.items}" @dbl-click="${this._onDblClick}"></paper-table>
-            
+            <confirm-delete></confirm-delete>
         `;
     }
 
---
+++ elements/confirm-delete/confirm-delete.js
@@ -0,0 +1,86 @@
+"use strict";
+import {LitElement, html, css} from '/node_modules/lit-element/lit-element.js';
+import './../paper-dialog/paper-dialog.js';
+import './../iron-ajax/iron-ajax.js';
+import './../paper-button/paper-button.js';
+
+class ConfirmDelete extends LitElement{
+
+    static get properties(){
+        return {
+            message: {
+                type: String
+            },
+            body: {
+                type: Object
+            },
+            callback: {
+                type: Function
+            },
+            url: {
+                type: String,
+                value: "/delete/DeleteEntity"
+            }
+        }
+    }
+
+    static get styles(){
+        return this.styleElement;
+    }
+
+    static get styleElement(){
+        // language=CSS
+        return css`
+            paper-dialog{
+                --min-dialog-width: 500px; 
+            }
+        `;
+    }
+    connectedCallback(){
+        super.connectedCallback();
+        document.addEventListener("confirm-delete", this._bindedConfirmDelete);
+    }
+    disconnectedCallback(){
+        super.disconnectedCallback();
+        document.removeEventListener("confirm-delete", this._bindedConfirmDelete);
+    }
+    constructor(){
+        super();
+        this._bindedConfirmDelete = this._listenForConfirmDelete.bind(this);
+    }
+
+    render(){
+        return html`
+            <iron-ajax .url="${this.url}" .body="${this.body}" @iron-response="${this.callback}"></iron-ajax>
+            <paper-dialog class="dialog" .noActions="${true}"> 
+                <div slot="header" class="header">Confirm</div>                  
+                <div slot="body">${this.message}</div>
+                <paper-button slot="button" icon="close" style="background: var(--grey-color)" @click="${this._closeDialog}">Cancel</paper-button>
+                <paper-button slot="button" icon="delete" style="background: var(--red-color)" @click="${this.confirmDelete}">Delete</paper-button>
+            </paper-dialog>
+        `;
+    }
+    _listenForConfirmDelete(event){
+        console.log(event.detail);
+        this.delete(event.detail.body, event.detail.message, event.detail.callback, event.detail.url);
+    }
+    delete(body, message, callback, url) {
+        this.body = body;
+        this.message = message;
+        this.callback = callback || function(){};
+        this.url = url || "/delete/DeleteEntity";
+        this._openDialog();
+    }
+    _openDialog(){
+        this.shadowRoot.querySelector('paper-dialog').open();
+    }
+    _closeDialog () {
+        this.shadowRoot.querySelector('paper-dialog').close();
+    }
+    confirmDelete(){
+        this.shadowRoot.querySelector('iron-ajax').generateRequest();
+    }
+
+}
+
+customElements.define("confirm-delete", ConfirmDelete);
