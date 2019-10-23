736b7022831be19bd4e5e34a512ce445dae9d482
octavian.voloaca <octavianvoloaca@gmail.com>
2019-10-23 17:01:18 +0300

    empty view

---
+++ elements/iron-views/empty-view.js
@@ -0,0 +1,56 @@
+"use strict";
+import {LitElement, html, css} from '/node_modules/lit-element/lit-element.js';
+import {flexLayoutClasses} from "./../flex-layout/flex-layout-classes.js";
+
+class EmptyView extends LitElement {
+
+    static get properties() {
+        return {
+            currentPage: {type: String},
+            name: {type: String}
+        };
+    }
+
+    static get styles(){
+        return [flexLayoutClasses, this.styleElement]
+    }
+
+    static get styleElement(){
+        // language=CSS
+        return css`
+            :host{
+                display: flex;
+            }
+        `
+    }
+
+    set currentPage(page){
+        this._currentPage = page;
+        this.refreshPage();
+    }
+
+    get currentPage(){
+        return this._currentPage;
+    }
+
+    firstUpdated(){
+        this.request = this.shadowRoot.querySelector('.request');
+        this.refreshPage();
+    }
+
+    render() {
+        return html`               
+            empty-view     
+        `;
+    }
+
+    refreshPage(){
+        if(this.request !== undefined && this.name === this.currentPage){
+            this.request.generateRequest();
+        }
+    }
+
+}
+customElements.define("empty-view", EmptyView);
+
+
