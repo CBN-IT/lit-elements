0f8b45d6b40f589f146f3907e27b5a2712d7414f
Bogdan Nourescu <bogdan.nourescu@cbn-it.ro>
2020-05-04 15:37:22 +0300

    removed flex from the body of paper-dialog to be able to calculate the height correctly

--- elements/paper-dialog/paper-dialog.js
+++ elements/paper-dialog/paper-dialog.js
@@ -98,7 +98,7 @@ class PaperDialog extends LitElement {
                     </h3>
                     <paper-button small class="close-button" small icon="close" @click="${this._onCancelClick}"></paper-button>
                 </div>
-                <div id="body" class="flex">
+                <div id="body">
                     <slot name="body"></slot>
                 </div>
                <div id="buttons" class="horizontal layout justified">
