765fd3d958965c6d8ff94f2b592412b564c526c7
Bogdan Nourescu <bogdan.nourescu@scoalainformala.ro>
2021-05-17 14:18:11 +0300

    update TableViewNoLink

--- elements/iron-views/table-view-no-link.js
+++ elements/iron-views/table-view-no-link.js
@@ -3,10 +3,12 @@ import {LitElement, html, css} from 'lit-element';
 import {flexLayoutClasses} from "../flex-layout/flex-layout-classes.js";
 import "../iron-form/iron-form.js";
 import "../paper-table/paper-table.js";
-import "./../paper-fab/paper-fab.js";
-import "./../paper-dialog/paper-dialog.js";
+import "../paper-fab/paper-fab.js";
+import "../paper-dialog/paper-dialog.js";
+import "../paper-reports-dropdown/paper-reports-dropdown.js";
 
 import "add|../iron-icons/icons.svgicon";
+import "excel|../iron-icons/cbn.svgicon";
 
 
 export class TableViewNoLink extends LitElement {
@@ -15,9 +17,14 @@ export class TableViewNoLink extends LitElement {
         return {
             currentPage: {type: String},
             name: {type: String},
+            getUrl: {type: String},
             collection: {type: String},
             columns: {type: Array},
-            items: {type: Array}
+            items: {type: Array},
+            reports: {type: Array},
+            addView: {type: String},
+            deleteUrl: {type: String},
+            disabledRequest: {type: Boolean}
         };
     }
 
@@ -30,15 +37,25 @@ export class TableViewNoLink extends LitElement {
         return css`
             :host{
                 display: flex;
+                flex-direction: column;
+                position: relative;
             }
-            paper-fab[icon="add"]{
+
+            paper-fab {
                 bottom: 10px;
                 right: 10px;
                 background: var(--app-secondary-color)
             }
-            paper-fab[icon="excel"]{
-                bottom: 10px;
-                right: 50px;
+
+            .paper-material {
+                box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+                background: white;
+                border-radius: 5px;
+                margin: 10px;
+            }
+
+            .top-bar {
+                margin-bottom: 0;
             }
             [icon="excel"] {
                 --iron-icon-color: #207245;
@@ -46,12 +63,26 @@ export class TableViewNoLink extends LitElement {
         `
     }
 
-    constructor() {
-        super();
-        this.collection = 'document';
-        this.columns = [];
-        this.items = [];
-    }
+        constructor() {
+            super();
+            this.items = [];
+            this.getUrl = '/GetDocuments';
+            this.addView = 'add-with-link';
+            this.deleteUrl = "/DeleteItem";
+        }
+
+        set collection(value) {
+            this._collection = value;
+            this.config = window.data._configs[this._collection];
+            this.columns = window.data._columns[this._collection];
+            this.reports = window.data._reports
+                .filter(item => item.collection === this._collection)
+                .sort((a, b) => a.reportName.localeCompare(b.reportName));
+        }
+
+        get collection() {
+            return this._collection;
+        }
 
     shouldUpdate(changedProperties) {
         if (changedProperties.has('currentPage')) {
@@ -61,12 +92,22 @@ export class TableViewNoLink extends LitElement {
     }
 
     firstUpdated(){
+        this.table = this.shadowRoot.querySelector('paper-table');
         this.request = this.shadowRoot.querySelector('.request');
     }
 
     render() {
         return html`               
-            <iron-ajax class="request" url="/GetDocuments" .params="${{'collection': this.collection}}" @iron-response="${this._onIronResponse}"></iron-ajax>  
+            <iron-ajax class="request" url="${this.getUrl}" .params="${{'collection': this.collection}}" @iron-response="${this._onIronResponse}"></iron-ajax>  
+            <div class="horizontal layout paper-material top-bar center">
+                <paper-button icon="add" @click="${this._addDocument}" style="background: var(--green-color)">Adauga</paper-button>
+                ${this._displayReportsDropdown()}
+                <div class="flex horizontal layout center">
+                    ${this._getTopBarTemplate()}
+                </div>
+                <paper-button title="Salveaza ca Excel" icon="excel" small @click="${this.saveAsExcel}"></paper-button>
+                <div style="width:44px;"></div>
+            </div>
             <paper-table
                 class="flex paper-material" 
                 .columns="${this.columns}" 
@@ -75,28 +116,28 @@ export class TableViewNoLink extends LitElement {
                 @delete-item="${this._deleteItem}"
                 @cbn-table-select="${this._onTableSelect}"
             ></paper-table>
-            <paper-fab icon="add" @click="${this._addDocument}"></paper-fab>
-            <paper-fab icon="excel" @click="${this._addDocument}"></paper-fab>
         `;
     }
     _onTableSelect(event){
-
+    }
+    _getTopBarTemplate() {
+        return '';
     }
     async saveAsExcel(){
         CBNUtils.startLoading();
         await this.table.saveXls();
         CBNUtils.stopLoading();
     }
-    _addDocument(){
+    _displayReportsDropdown() {
+        return this.reports && this.reports.length > 0 ? html`<paper-reports-dropdown .options="${this.reports}" .table="${this.table}"></paper-reports-dropdown>` : '';
+    }
+
+    async _addDocument(){
         CBNUtils.fireEvent(this, 'show-page', {page: 'add-no-link'});
     }
 
-    _onDblClick(event){
+    async _onDblClick(event){
         CBNUtils.fireEvent(this, 'add-no-link', {page: 'add-no-link', model: event.detail.item});
-    }
-
-    _onIronResponse(event){
-        this.items = event.detail.response;
     }
 
     refreshPage(newPage, oldPage){
@@ -106,9 +147,25 @@ export class TableViewNoLink extends LitElement {
     }
 
     async _getItems() {
-        if (this.request) {
+        if (this.request && !this.disabledRequest) {
             this.items = await this.request.generateRequest();
         }
+    }
+
+    async _deleteItem(event) {
+        CBNUtils.fireEvent(this, 'confirm-delete', {
+            message: 'Esti sigur ca doresti sa stergi aceasta inregistrare?',
+            url: this.deleteUrl,
+            body: {
+                _id: event.detail._id,
+                collection: this.collection
+            },
+            callback: this._deletedItem.bind(this)
+        });
+    }
+
+    _deletedItem() {
+        this._getItems();
     }
 
 }
