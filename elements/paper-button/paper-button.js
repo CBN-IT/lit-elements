cd8b356a7084737924da241056a6377a260a4c25
didar <disu.lut@gmail.com>
2024-09-09 17:06:03 +0300

    added a tooltip slot to paper-button element

--- elements/paper-button/paper-button.js
+++ elements/paper-button/paper-button.js
@@ -11,8 +11,8 @@ export class PaperButton extends LitElement {
     static get properties() {
         return {
             icon: {type: String},
-            iconSize:{type:Number},
-            svgIcon: {type: Object}
+            iconSize: {type: Number},
+            svgIcon: {type: Object},
         };
     }
 
@@ -26,7 +26,6 @@ export class PaperButton extends LitElement {
             :host {
                 display: inline-flex;
                 position: relative;
-                overflow: hidden;
                 height: 24px;
                 margin: 10px;
                 user-select: none;
@@ -40,7 +39,12 @@ export class PaperButton extends LitElement {
                 color: var(--black-color, black);
             }
 
-            :host:after {
+            .container {
+                overflow: hidden;
+                position: relative;
+            }
+
+            .container:after {
                 content: "";
                 display: block;
                 position: absolute;
@@ -57,13 +61,13 @@ export class PaperButton extends LitElement {
                 transition: transform .1s, opacity 1s;
             }
 
-            :host(:active):after {
+            .container:active:after {
                 transform: scale(0, 0);
                 opacity: .5;
                 transition: 0s;
             }
 
-            :host(:hover) {
+            .container:hover {
                 box-shadow: 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12), 0 3px 5px -1px rgba(0, 0, 0, 0.4);
                 cursor: pointer;
             }
@@ -115,18 +119,91 @@ export class PaperButton extends LitElement {
                     transform: rotate(360deg);
                 }
             }
+
+            .tooltip-container {
+                position: absolute;
+                top: calc(100% + 10px);
+                left: 50%;
+                transform: translateX(-50%);
+                width: auto;
+                padding: 2px 5px;
+                background-color: rgb(38, 42, 57);
+                color: white;
+                font-size: 14px;
+                box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12), 0 2px 4px -1px rgba(0, 0, 0, 0.3);
+                border-radius: 5px;
+                display: none;
+                z-index: 1000;
+                text-align: center;
+                white-space: nowrap;
+            }
+
+            .tooltip-container::after {
+                content: '';
+                position: absolute;
+                top: -14px;
+                left: 50%;
+                margin-left: -8px;
+                border-width: 8px;
+                border-style: solid;
+                border-color: transparent transparent rgb(38, 42, 57) transparent;
+            }
+
+
+            :host(:hover) .tooltip-container {
+                display: block;
+            }
+
+            :host(.tooltip-right) .tooltip-container::after {
+                left: 10px;
+                margin-left: 0;
+            }
+
+            :host(.tooltip-right) .tooltip-container {
+                left: 0;
+                transform: translateX(0);
+            }
+
+            :host(.tooltip-left) .tooltip-container::after {
+                left: initial;
+                right: 10px;
+                margin-left: 0;
+            }
+
+            :host(.tooltip-left) .tooltip-container {
+                left: 100%;
+                transform: translateX(-100%);
+            }
+
+            :host(:hover) .tooltip-container.hidden {
+                display: none;
+            }
+
         `
     }
 
     render() {
-        return html`           
+        return html`
             <div class="container horizontal layout center">
-                <iron-icon icon="${this.icon}" .svgIcon="${this.svgIcon}" .size="${this.iconSize}"></iron-icon>             
+                <iron-icon icon="${this.icon}" .svgIcon="${this.svgIcon}" .size="${this.iconSize}"></iron-icon>
                 <div class="text">
                     <slot></slot>
-                </div>              
+                </div>
             </div>
+            <slot name="tooltip" class="tooltip-container hidden" @slotchange="${this.handleSlotChange}"></slot>
+
         `;
+    }
+
+    handleSlotChange(event) {
+        let slot = event.target;
+        let hasTooltipSlot = slot.assignedElements().length !== 0;
+
+        if(!hasTooltipSlot){
+            slot.classList.add("hidden")
+        } else{
+            slot.classList.remove("hidden")
+        }
     }
 
 }
